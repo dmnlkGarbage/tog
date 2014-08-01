6be89a82730d30cbadb7a8be12f2e3d181c43355
Masaki Kawaguchi <seikima2demon@gmail.com>
2014-08-02 01:26:17 +0900

    init !

---
+++ src/main.go
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: masaki
+ * Date: 2014/08/02
+ * Time: 1:25
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+}
