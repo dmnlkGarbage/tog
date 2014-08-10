34cd942f6b4cfd2f1371b759a030c07a8653a4b9
Masaki Kawaguchi <seikima2demon@gmail.com>
2014-08-11 00:26:28 +0900

    start Tour of go

---
+++ src/tog01.go
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: masaki
+ * Date: 2014/08/11
+ * Time: 0:19
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import (
+	"fmt"
+)
+func  main() {
+	fmt.Println("Hello, world")
+}
