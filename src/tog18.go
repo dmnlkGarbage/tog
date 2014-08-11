28f34fac7be02efc7667f534ba74c6cb48d8aa6c
川口　将貴 <m-kawaguchi@gcrest.com>
2014-08-11 20:14:29 +0900

    add tour of go 18

---
+++ src/tog18.go
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m00217-kawaguchi
+ * Date: 2014/08/11
+ * Time: 20:12
+ * To change this template use File | Settings | File Templates.
+ */
+package tog18
+
