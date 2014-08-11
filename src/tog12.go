eafbac266900166873662799ac661e46a444007d
川口　将貴 <m-kawaguchi@gcrest.com>
2014-08-11 19:47:12 +0900

    add tour of go 12

---
+++ src/tog12.go
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m00217-kawaguchi
+ * Date: 2014/08/11
+ * Time: 19:42
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import (
+	"fmt"
+)
+
+var	i, j  int = 1, 3
+var c, python, java = false, true, "false"
+func main() {
+	fmt.Println(i, j)
+	fmt.Println(c, python, java)
+}
+
