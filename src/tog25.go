0ea65fdb315e5fabcfa869b949ee63ac32118176
川口　将貴 <m-kawaguchi@gcrest.com>
2014-08-11 20:38:21 +0900

    add tour of go 25

---
+++ src/tog25.go
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m00217-kawaguchi
+ * Date: 2014/08/11
+ * Time: 20:37
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	fmt.Println(Vertex{1, 2})
+}
