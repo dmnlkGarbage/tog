6d543a44719ad7f0802827621aa5507857a35ba3
川口　将貴 <m-kawaguchi@gcrest.com>
2014-08-11 20:18:13 +0900

    add tour of go 20

---
+++ src/tog20.go
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m00217-kawaguchi
+ * Date: 2014/08/11
+ * Time: 20:17
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+func main() {
+	for{
+		
+	}
+}
+
