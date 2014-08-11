57cbe0379c3ee9caeeb772b57ca5300fe8a81691
川口　将貴 <m-kawaguchi@gcrest.com>
2014-08-11 19:42:07 +0900

    add tour of go 11

---
+++ src/tog11.go
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m00217-kawaguchi
+ * Date: 2014/08/11
+ * Time: 19:40
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import (
+	"fmt"
+)
+
+var	 i  int
+var c, python, java bool
+func main() {
+	fmt.Println(i, c, python, java)
+}
+
