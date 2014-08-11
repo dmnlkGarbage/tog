8b03e25a105f6eb8c910000e2d8c8a2e20f13966
川口　将貴 <m-kawaguchi@gcrest.com>
2014-08-11 19:21:12 +0900

    add tour of go 05

---
+++ src/tog05.go
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m00217-kawaguchi
+ * Date: 2014/08/11
+ * Time: 19:18
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import(
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("now you hava %g problems.", math.Nextafter(2, 3))
+}
+
