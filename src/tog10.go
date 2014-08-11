e55836f0d86b2c7d404a1371ea3580d86c033e09
川口　将貴 <m-kawaguchi@gcrest.com>
2014-08-11 19:39:01 +0900

    add tour of go 10

---
+++ src/tog10.go
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m00217-kawaguchi
+ * Date: 2014/08/11
+ * Time: 19:35
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func split(sum int) (x, y int) {
+	x = sum * 4
+	y = sum -x
+	return
+}
+
+
+func main() {
+	fmt.Println(split(10))
+}
