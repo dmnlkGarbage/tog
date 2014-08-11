b8f7c6f363e6a57e9e7b8737a7d66d89f5a43496
川口　将貴 <m-kawaguchi@gcrest.com>
2014-08-11 19:31:28 +0900

    add tour of go 08

---
+++ src/tog08.go
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m00217-kawaguchi
+ * Date: 2014/08/11
+ * Time: 19:25
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func add(x ,y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(2, 5))
+}
+
