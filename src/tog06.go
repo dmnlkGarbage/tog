6209981699456949b283dbc2c58dbce0a88a42b6
川口　将貴 <m-kawaguchi@gcrest.com>
2014-08-11 19:24:19 +0900

    add tour of go 06

---
+++ src/tog06.go
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m00217-kawaguchi
+ * Date: 2014/08/11
+ * Time: 19:23
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.pi)
+}
+
