07860ddb13c999845b99cc7a493e5e6c41d5e042
Masaki Kawaguchi <seikima2demon@gmail.com>
2014-08-18 01:06:11 +0900

    add tour of go 38

--- src/tog38.go
+++ src/tog38.go
@@ -2,7 +2,7 @@
  * Created with IntelliJ IDEA.
  * User: masaki
  * Date: 2014/08/18
- * Time: 0:44
+ * Time: 1:02
  * To change this template use File | Settings | File Templates.
  */
 package main
@@ -13,15 +13,15 @@ type Vertex struct {
 	Lat, Long float64
 }
 
-var m map[string]Vertex
-
-func main() {
-	m = make (map[string]Vertex)
-	m["Bell labs"] = Vertex{
-		40.22221, -212.211,
-	}
-	fmt.Println(m["Bell labs"])
+var m = map[string]Vertex{
+	"Bell":Vertex{
+		11.11,22.32,
+	},
+	"Log":Vertex{
+		333.11,44.22,
+	},
 }
 
-
-
+func main() {
+	fmt.Println(m)
+}
