bd7238a6b2b608ab3200e53c89430f8491691109
dmnlk <seikima2demon@gmail.com>
2014-12-26 02:56:01 +0900

    fix comment

--- src/tog41.go
+++ src/tog41.go
@@ -6,9 +6,11 @@ import (
 )
 
 func WordCount(s string) map[string]int {
+	//map作る
 	m := make(map[string] int)
+	//まず空白区切りでバラす
 	val := strings.Fields(s)
-
+	//forで回して、既にある単語ならインクリメント
 	for _, v := range val {
 		val, ok := m[v]
 		if ok {
