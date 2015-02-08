95e6f0702481c1d35bbcc399d5715ae64f8dcea2
dmnlk <seikima2demon@gmail.com>
2015-02-09 00:01:57 +0900

    fix #4

--- src/tog58.go
+++ src/tog58.go
@@ -7,7 +7,10 @@ import (
 
 func main() {
 	var s String = "aaaaa"
-	http.ListenAndServe("localhost:4000", s)
+
+	http.Handle("/struct", &Struct{"hello", ":", "me"})
+	http.Handle("/str", s)
+	http.ListenAndServe("localhost:4000", nil)
 }
 
 
@@ -22,4 +25,8 @@ type Struct struct {
 	Greeting string
 	Punct    string
 	Who      string
+}
+
+func (s Struct) ServeHTTP(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintf(w, s.Greeting + s.Punct + s.Who)
 }
